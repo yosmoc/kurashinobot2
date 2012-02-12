13b65b4baa0e2a320cb8c3325a9aa658ed1db448
samurai20000 <samurai20000___@___gmail.com>
2012-02-12 23:48:09 +0900

    postできるようにした

--- bot.js
+++ bot.js
@@ -1,30 +1,46 @@
 "use strict";
 
 var path = require('path'),
+sys = require('sys'),
     fs = require('fs'),
     DB = require('./lib/DB').DB,
     cronJob = require('cron').CronJob,
     confy = require('confy'),
+    twitter = require('twitter'),
     undefined;
 
 function Bot() {
     this.db = new DB(path.resolve() + '/db/kurashi_no_hint');
+
+    var self = this;
     this.conf = confy.get('twitter.com', { require: {
         consumer_key: '',
         consumer_secret: '',
         access_token_key: '',
         access_token_secret: ''    
     }}, function(err, result) {
-        console.log(result);
+        self.bot = new twitter({
+            consumer_key: result.consumer_key,
+            consumer_secret: result.consumer_secret,
+            access_token_key: result.access_token_key,
+            access_token_secret: result.access_token_secret
+        });
     });
-    console.log(this.conf);
 }
 
 Bot.prototype = {
     say: function() {
         var hint = this.db.hint_queue.shift();
+        // this.bot.verifyCredentials(function (data) {
+        //     sys.puts(sys.inspect(data));
+        // }).updateStatus(hint.hint, function(data) {
+        //     sys.puts(sys.inspect(data));
+        // });
+        this.bot.updateStatus(hint.hint, function(data) {
+             sys.puts(sys.inspect(data));
+        });
+
         this.db.insert_queue_random();
-        console.log(hint);
     }
 }
 
