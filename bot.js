94ffb2e14322c182bff4e06144d1bb9945fdd28f
samurai20000 <samurai20000___@___gmail.com>
2012-02-12 22:59:24 +0900

    アカウント管理はconfyで行うことにする

--- bot.js
+++ bot.js
@@ -4,10 +4,20 @@ var path = require('path'),
     fs = require('fs'),
     DB = require('./lib/DB').DB,
     cronJob = require('cron').CronJob,
+    confy = require('confy'),
     undefined;
 
 function Bot() {
     this.db = new DB(path.resolve() + '/db/kurashi_no_hint');
+    this.conf = confy.get('twitter.com', { require: {
+        consumer_key: '',
+        consumer_secret: '',
+        access_token_key: '',
+        access_token_secret: ''    
+    }}, function(err, result) {
+        console.log(result);
+    });
+    console.log(this.conf);
 }
 
 Bot.prototype = {
