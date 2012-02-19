60e8f4dcbee9b496204a882ede07290db1bc9ea7
samurai20000 <samurai20000___@___gmail.com>
2012-02-19 10:07:04 +0900

    confy依存をやめて、ENVを使用することに変更

--- bot.js
+++ bot.js
@@ -1,30 +1,21 @@
 "use strict";
 
 var path = require('path'),
-sys = require('sys'),
+    sys = require('sys'),
     fs = require('fs'),
     DB = require('./lib/DB').DB,
     cronJob = require('cron').CronJob,
-    confy = require('confy'),
     twitter = require('twitter'),
     undefined;
 
 function Bot() {
     this.db = new DB(path.resolve() + '/db/kurashi_no_hint');
 
-    var self = this;
-    this.conf = confy.get('twitter.com', { require: {
-        consumer_key: '',
-        consumer_secret: '',
-        access_token_key: '',
-        access_token_secret: ''    
-    }}, function(err, result) {
-        self.bot = new twitter({
-            consumer_key: result.consumer_key,
-            consumer_secret: result.consumer_secret,
-            access_token_key: result.access_token_key,
-            access_token_secret: result.access_token_secret
-        });
+    this.bot = new twitter({
+        consumer_key: process.env[CONSUMER_KEY],
+        consumer_secret: process.env[CONSUMER_SECRET],
+        access_token_key: process.env[ACCESS_TOKEN_KEY],
+        access_token_secret: process.env[ACCESS_TOKEN_SECRET]
     });
 }
 
