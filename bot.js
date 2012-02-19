55c694069e95d5ef10ec012f69d3377a6b3a06b7
samurai20000 <samurai20000___@___gmail.com>
2012-02-19 15:21:28 +0900

    queueのinitializeはDB.jsからではなく、bot.jsからすることに変更

--- bot.js
+++ bot.js
@@ -18,6 +18,8 @@ function Bot() {
         access_token_key: process.env.ACCESS_TOKEN_KEY,
         access_token_secret: process.env.ACCESS_TOKEN_SECRET
     });
+
+    this.db.queue_initialize();
 }
 
 Bot.prototype = {
