fff1698fc0b863ee6527c311d8a477f4ab1f6d46
samurai20000 <samurai20000___@___gmail.com>
2012-02-19 10:24:20 +0900

    process.env[]の形からprocess.env.hogeの形に変更

--- bot.js
+++ bot.js
@@ -13,10 +13,10 @@ function Bot() {
     this.db = new DB(path.resolve() + '/db/kurashi_no_hint');
 
     this.bot = new twitter({
-        consumer_key: process.env[CONSUMER_KEY],
-        consumer_secret: process.env[CONSUMER_SECRET],
-        access_token_key: process.env[ACCESS_TOKEN_KEY],
-        access_token_secret: process.env[ACCESS_TOKEN_SECRET]
+        consumer_key: process.env.CONSUMER_KEY,
+        consumer_secret: process.env.CONSUMER_SECRET,
+        access_token_key: process.env.ACCESS_TOKEN_KEY,
+        access_token_secret: process.env.ACCESS_TOKEN_SECRET
     });
 }
 
